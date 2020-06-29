974e60539c133a3404050857d9c5318c654e25b6
Rachel Williams <rachelwilliams659@gmail.com>
2020-06-29 17:19:38 -0600

    Add Turn tests for function, class instance, and guess property

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -2,10 +2,34 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const Turn = require('../src/Turn');
+const Card = require('../src/Card');
 
 describe('Turn', function() {
 
+  it.skip('should be a function', function() {
+    const turn = new Turn();
+    expect(Turn).to.be.a('function');
+  });
 
+  it.skip('should be an instance of Turn', function() {
+    const turn = new Turn(); 
+    expect(turn).to.be.an.instanceof(Turn); 
+  });
 
+  it.skip('should store a guess', function() {
+    const card = new Card(4, 'What type of prototype method does not modify the existing array but returns a particular representation of the array?', ['mutator method', 'accessor method', 'iteration method'], 'accessor method'); 
+    const turn = new Turn('mutator method', card);
+    expect(turn.guess).to.equal('mutator method');
+  });
 
 }); 
+
+
+
+// Example data:
+// {
+//   "id": 4,
+//   "question": "What type of prototype method does not modify the existing array but returns a particular representation of the array?",
+//   "answers": ["mutator method", "accessor method", "iteration method"],
+//   "correctAnswer": "accessor method"
+// }
