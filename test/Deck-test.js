86ac3fc2b98b912ced2c6545806382bd5d74ae56
Rachel Williams <rachelwilliams659@gmail.com>
2020-06-29 19:51:59 -0600

    Create Deck class based on test file

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -2,20 +2,21 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const Deck = require('../src/Deck');
+const Card = require('../src/Card')
 
 describe('Deck', function() {
 
-  it.skip('should be a function', function() {
+  it('should be a function', function() {
     const deck = new Deck();
     expect(Deck).to.be.a('function')
   });
 
-  it.skip('should be an instance of Deck', function() {
+  it('should be an instance of Deck', function() {
     const deck = new Deck(); 
     expect(deck).to.be.an.instanceof(Deck);
   });
  
-  it.skip('should store a deck of cards', function() {
+  it('should store a deck of cards', function() {
     const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
     const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
     const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
@@ -23,7 +24,7 @@ describe('Deck', function() {
     expect(deck.cards).to.deep.equal([card1, card2, card3]); 
   });
 
-  it.skip('should know how many cards are in the deck', function() {
+  it('should know how many cards are in the deck', function() {
     const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
     const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
     const deck = new Deck([card1, card2]);
