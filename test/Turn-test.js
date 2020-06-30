b58e505bc0c0537a07d460bbe1289af3689188dd
Rachel Williams <rachelwilliams659@gmail.com>
2020-06-29 19:12:29 -0600

    Refactor Turn test with beforeEach

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -5,6 +5,10 @@ const Turn = require('../src/Turn');
 const Card = require('../src/Card');
 
 describe('Turn', function() {
+  let card; 
+  beforeEach(function() {
+    card = new Card(4, 'What type of prototype method does not modify the existing array but returns a particular representation of the array?', ['mutator method', 'accessor method', 'iteration method'], 'accessor method'); 
+  });
 
   it('should be a function', function() {
     const turn = new Turn();
@@ -17,50 +21,41 @@ describe('Turn', function() {
   });
 
   it('should store a guess', function() {
-    const card = new Card(4, 'What type of prototype method does not modify the existing array but returns a particular representation of the array?', ['mutator method', 'accessor method', 'iteration method'], 'accessor method'); 
     const turn = new Turn('mutator method', card);
     expect(turn.guess).to.equal('mutator method');
   });
 
   it('should store a card', function() {
-    const card = new Card(4, 'What type of prototype method does not modify the existing array but returns a particular representation of the array?', ['mutator method', 'accessor method', 'iteration method'], 'accessor method');
     const turn = new Turn('mutator method', card);
     expect(turn.currentCard).to.deep.equal(card);  
   });
 
   it('should be able to return the current card', function() {
-    const card = new Card(4, 'What type of prototype method does not modify the existing array but returns a particular representation of the array?', ['mutator method', 'accessor method', 'iteration method'], 'accessor method');
     const turn = new Turn('mutator method', card);
     expect(turn.returnGuess()).to.equal('mutator method'); 
   });
 
   it('should be able to return the guess', function() {
-    const card = new Card(4, 'What type of prototype method does not modify the existing array but returns a particular representation of the array?', ['mutator method', 'accessor method', 'iteration method'], 'accessor method');
     const turn = new Turn('mutator method', card);
     expect(turn.returnCard()).to.deep.equal(card);
   }); 
 
   it('should be able to evaluate the guess and return true if guess is correct', function() {
-    const card = new Card(4, 'What type of prototype method does not modify the existing array but returns a particular representation of the array?', ['mutator method', 'accessor method', 'iteration method'], 'accessor method');
     const turn = new Turn('accessor method', card);
     expect(turn.evaluateGuess()).to.equal(true);     
   });
 
   it('should return false if guess is incorrect', function() {
-    const card = new Card(4, 'What type of prototype method does not modify the existing array but returns a particular representation of the array?', ['mutator method', 'accessor method', 'iteration method'], 'accessor method');
     const turn = new Turn('mutator method', card);  
     expect(turn.evaluateGuess()).to.equal(false); 
   });
 
-  //test giveFeedback property 
   it('should indicate if guess is correct', function() {
-    const card = new Card(4, 'What type of prototype method does not modify the existing array but returns a particular representation of the array?', ['mutator method', 'accessor method', 'iteration method'], 'accessor method');
     const turn = new Turn('accessor method', card);
     expect(turn.giveFeedback()).to.equal('Correct!');    
   });
 
   it('should indicate if guess is incorrect', function() {
-    const card = new Card(4, 'What type of prototype method does not modify the existing array but returns a particular representation of the array?', ['mutator method', 'accessor method', 'iteration method'], 'accessor method');
     const turn = new Turn('mutator method', card);
     expect(turn.giveFeedback()).to.equal('Incorrect!');    
   });
