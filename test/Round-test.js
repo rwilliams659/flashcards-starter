a2b97510d6a3994a20f926bd7b7e4b783ecd3e75
Rachel Williams <rachelwilliams659@gmail.com>
2020-06-30 15:43:05 -0600

    Add 2 Round tests for takeTurn method

--- test/Round-test.js
+++ test/Round-test.js
@@ -7,6 +7,14 @@ const Turn = require('../src/Turn');
 const Card = require('../src/Card');
 
 describe('Round', function() {
+  let card1;
+  let card2;
+  let deck; 
+  beforeEach(function() {
+    let card1 = new Card(3, 'What type of prototype method directly modifies the existing array?', ['mutator method', 'accessor method', 'iteration method'], 'mutator method');
+    let card2 = new Card(7, 'Which array prototype is not an accessor method?', ['join()', 'slice()', 'splice()'], 'splice()');
+    let deck = new Deck([card1, card2]); 
+  });
 
   it.skip('should be a function', function() {
     const round = new Round();
@@ -19,15 +27,26 @@ describe('Round', function() {
   });
 
   it.skip('should store a deck of cards', function() {
-    const card1 = new Card(3, 'What type of prototype method directly modifies the existing array?', ['mutator method', 'accessor method', 'iteration method'], 'mutator method');
-    const card2 = new Card(7, 'Which array prototype is not an accessor method?', ['join()', 'slice()', 'splice()'], 'splice()');
-    const deck = new Deck([card1, card2]); 
     const round = new Round(deck);
     expect(round.deck).to.equal(deck)
   });
 
-  //multiple tests needed for takeTurn method; first: 
-    //takeTurn method must take in a guess 
-    //it should create new instance of Turn & pass in guess & current card 
+  //the below block may need editing
+  it.skip('should instantiate a new turn', function() {
+    const round = new Round(deck);
+    
+    round.takeTurn('mutator method');
+
+    expect(turn).to.be.an.instanceof(Turn); 
+  });
+
+  it.skip('should keep track of how many turns have been taken', function() {
+    const round = new Round(deck);
+
+    round.takeTurn('mutator method');
+    round.takeTurn('slice()');
+
+    expect(round.turns).to.equal(2)
+  });
 
 })
